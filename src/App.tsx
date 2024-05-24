8d13b5badf2b83ee837e4f6c432a0ca80a81070a
alexvedia <alexvediacastro@gmail.com>
2024-05-24 14:20:45 -0400

    header y main iniciado, ingresando a los componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
+import { menuItems } from "./data/db"
 
 function App() {
-
+  console.log(menuItems)
   return (
     <>
-<h1>Hello world in react</h1>
+      <header className="bg-teal-400 py-5">
+        <h1 className="text-center text-4xl font-black">Calculadora de propinas y consumo</h1>
+      </header>
+
+      <main className="max-w-7xl mx-auto py-20 grid md:grid-cols-2">
+        <div>
+          <h2>Menu</h2>
+        </div>
+        <div>
+          <h2>Consumo</h2>
+        </div>
+      </main>
     </>
   )
 }
---
+++ src/types/index.ts
@@ -0,0 +1,5 @@
+export type MenuItem = {
+    id: number,
+    name: string,
+    price: number
+}
